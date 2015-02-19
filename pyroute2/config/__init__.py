5d1184d94fb66bbb9a66cd9c6732cf607206c9e9
Peter V. Saveliev <peter@svinota.eu>
2015-02-19 13:22:33 +0100

    config: make a separate module

--- pyroute2/config/__init__.py
+++ pyroute2/config/__init__.py
@@ -1,6 +1,7 @@
 import socket
 import multiprocessing
 
+# classes that one can to substitute if needed
 SocketBase = socket.socket
 MpPipe = multiprocessing.Pipe
 MpProcess = multiprocessing.Process
